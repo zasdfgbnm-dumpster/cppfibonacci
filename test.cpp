48070531e22df99fd128091676a8efb7bbf69651
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-10-21 21:59:43 -0400

    random fh generator

--- test.cpp
+++ test.cpp
@@ -3,8 +3,13 @@
 #include <vector>
 #include <functional>
 #include <tuple>
+#include <random>
+#include <algorithm>
+#include <limits>
 #define FIBONACCI_HEAP_TEST_FRIEND fibonacci_test
 #include "fibonacci.hpp"
+
+namespace {
 
 using namespace std;
 
@@ -16,7 +21,7 @@ class fibonacci_test {
 
 public:
 
-	/** \brief the class containing functions for white box test on fibonacci_heap
+	/** \brief the class containing functions for white box test
 	 *
 	 * @param K the type for keys
 	 * @param T the type for data
@@ -178,14 +183,14 @@ public:
 		/** \brief test the consistency of the forest of min trees maintained inside fibonacci_heap
 		*
 		* The following things are tested:
-		* 1 parent pointer
-		* 2 sibling pointers
-		* 3 degrees
-		* 4 data and structure pointers
-		* 5 min-tree property
-		* 6 min pointer of Fibonacci heap
-		* 7 _size
-		* 8 max_degree
+		* 1. parent pointer
+		* 2. sibling pointers
+		* 3. degrees
+		* 4. data and structure pointers
+		* 5. min-tree property
+		* 6. min pointer of Fibonacci heap
+		* 7. size
+		* 8. max_degree
 		*/
 		static void data_structure_consistency_test(const fh_t &fh) {
 			// test for 1-5
@@ -296,3 +301,129 @@ public:
 		}
 	};
 };
+
+/** \brief an engine to do random operations and generate random Fibonacci heaps */
+class random_engine {
+	using fh_t = fibonacci_heap<int,int>;
+	default_random_engine rng;
+	uniform_real_distribution<double> u01 = uniform_real_distribution<double>(0,1);
+	uniform_int_distribution<int> ui01 = uniform_int_distribution<int>(0,1);
+	uniform_int_distribution<int> uint = uniform_int_distribution<int>(numeric_limits<int>::min(),numeric_limits<int>::max());
+	shared_ptr<fh_t> fh[2];
+	vector<fh_t::node> nodes[2];
+public:
+
+	double pnew = 0.1;
+	double pcopy = 0.5;
+	double pdestroy = 0.05;
+	double pmeld = 0.1;
+	double premoveany = 0.5;
+	double pdecreasekey = 0.5;
+
+	/** \brief the probability that a Fibonacci heap has a given size */
+	virtual double probability(size_t size) = 0;
+
+	/** \brief make a random step */
+	virtual void random_step() {
+		// initialize or nothing
+		if(fh[0]==nullptr&&fh[1]==nullptr) {
+			fh[0] = make_shared<fh_t>();
+			return;
+		}
+		// meld or nothing
+		if(fh[0]!=nullptr && fh[1]!=nullptr && u01(rng)<pmeld) {
+			int i = ui01(rng);
+			fh[i]->meld(*fh[1-i]);
+			nodes[i].insert(nodes[i].end(),nodes[1-i].begin(),nodes[1-i].end());
+			nodes[1-i].clear();
+			return;
+		}
+		// destroy one or nothing
+		if(fh[0]!=nullptr && fh[1]!=nullptr && u01(rng)<pdestroy) {
+			int i = ui01(rng);
+			fh[i].reset();
+			nodes[i].clear();
+			return;
+		}
+		// create new or nothing
+		int i = ui01(rng);
+		if(fh[i]==nullptr && u01(rng)<pnew) {
+			if(u01(rng)<pcopy)
+				fh[i] = make_shared<fh_t>(move(*fh[1-i]));
+			else
+				fh[i] = make_shared<fh_t>();
+			return;
+		}
+		if(fh[i]==nullptr)
+			i = 1-i;
+		// insert, remove, remove(node), or decrease_key
+		double movetype = u01(rng);
+		double acceptrate = 0;
+		if(movetype<0.5) // insert
+			acceptrate = probability(fh[i]->size()+1)/probability(fh[i]->size());
+		else
+			acceptrate = probability(fh[i]->size()-1)/probability(fh[i]->size());
+		if(u01(rng)<acceptrate) {
+			if(movetype<0.5) {
+				// insert
+				nodes[i].push_back(fh[i]->insert(uint(rng),uint(rng)));
+			} else {
+				if((!nodes[i].empty())&&u01(rng)<premoveany) {
+					// remove(node)
+					size_t s = nodes[i].size();
+					uniform_int_distribution<int> dist(0,s-1);
+					size_t rmpos = dist(rng);
+					fh[i]->remove(nodes[i][rmpos]);
+					nodes[i].erase(nodes[i].begin()+rmpos);
+				} else { // remove()
+					fh_t::node r = fh[i]->remove();
+					nodes[i].erase(remove(nodes[i].begin(),nodes[i].end(),r),nodes[i].end());
+				}
+			}
+		} else {
+			if((!nodes[i].empty())&&u01(rng)<pdecreasekey) {
+				// decrease_key(node)
+				size_t s = nodes[i].size();
+				uniform_int_distribution<int> dist(0,s-1);
+				fh_t::node n = nodes[i][dist(rng)];
+				uniform_int_distribution<int> dist2(numeric_limits<int>::min(),n.key());
+				fh[i]->decrease_key(n,dist2(rng));
+			} else {
+				// insert then remove min
+				nodes[i].push_back(fh[i]->insert(uint(rng),uint(rng)));
+				fh_t::node r = fh[i]->remove();
+				nodes[i].erase(remove(nodes[i].begin(),nodes[i].end(),r),nodes[i].end());
+			}
+		}
+	}
+};
+
+/** \brief run random operations and check consistency after each operation */
+TEST(whitebox,consistency) {
+
+}
+
+/** \brief randomly insert,remove min, meld elements and check if binomial heap
+ * properties are maintained after each operation */
+TEST(whitebox,binomial) {
+
+}
+
+/** \brief generate a random Fibonacci heap and test copy and move constructor and assignment operator */
+TEST(whitebox,copy_move) {
+
+}
+
+/** \brief generate a random Fibonacci heap and test if this heap destroy correctly */
+TEST(whitebox,destroy) {}
+
+/** \brief randomly insert, remove, change or merge some elements and see if
+ * Fibonacci heap can generate a sorted list of remaining elements*/
+TEST(blackbox,sort) {}
+
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
